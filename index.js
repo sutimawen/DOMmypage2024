37b0552296ea92e59ff92e6189d8526d4e412177
sutimawen <168176085+sutimawen@users.noreply.github.com>
2024-05-10 07:07:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
+let count = 0;
 function checkAnswer(answer) {
-    const result = document.getElementById('result');
-    if (answer === 'c') {
-        result.textContent = "ถูกต้อง คุณอยู่หน่วยงานข้อมูลบริหาร";
+    const result = document.getElementById('result1');
+    const result3 = document.getElementById('result3');
+    if (answer === 'd') {
+        result.textContent = "ถูกต้อง คุณอยู่หน่วยงานข้อมูลจ่าย";
         result.style.color = 'green';
+        count++;
     } else {
-        result.textContent = "คำตอบคุณผิด กรุณาตอบใหม่!!!";
+        result.textContent = "ตอบผิด กรุณาตอบใหม่";
         result.style.color = 'red';
     }
- }
- function checkAnswer1(answer) {
-    const result = document.getElementById('result1');
+ 
+
+}
+function checkAnswer1(answer) {
+    const result = document.getElementById('result2');
+    const result3 = document.getElementById('result3');
     if (answer === 'c') {
         result.textContent = "ถูกต้อง คุณอยู่คลังสุวรรณภูมิ";
         result.style.color = 'green';
+        count++;
     } else {
-        result.textContent = "คำตอบคุณผิด กรุณาตอบใหม่!!!";
+        result.textContent = "ตอบผิด กรุณาตอบใหม่";
         result.style.color = 'red';
     }
+    result3.textContent = "คุณได้ทั้งหมด " + count + " คะแนน";
+    count = 0;
  }
  
+
 
